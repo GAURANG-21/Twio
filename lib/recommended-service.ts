55a8c681cf66a4f67e6a59fb1d4b13d179790cb3
GAURANG-21 <gaurang2621@gmail.com>
2025-01-18 15:47:13 +0530

    feat: user cannot be recommended to itself

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -1,14 +1,36 @@
 import { db } from "@/lib/db";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { getSelf } from "@/lib/auth-service"; //* Will use later to not show user its own name in recommended
 
 export const getRecommended = async () => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  let self;
+
+  try {
+    self = await getSelf();
+    self = self.externalUserId;
+  } catch {
+    self = null;
+  }
+
+  let users = [];
+  if (self) {
+    users = await db.user.findMany({
+      where: {
+        NOT: {
+          externalUserId: self,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
 
   return users;
 };
