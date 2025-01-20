abd534676f66cec68265bd2a4130ee57c239ed08
GAURANG-21 <gaurang2621@gmail.com>
2025-01-20 14:35:12 +0530

    modify: blockerId -> blockedId to ensure that the users I follow haven't blocked me and return accordingly

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -104,7 +104,7 @@ export const getFollowedUsers = async () => {
               following: {
                 blocking: {
                   none: {
-                    blockerId: self.id,
+                    blockedId: self.id,
                   },
                 },
               },
