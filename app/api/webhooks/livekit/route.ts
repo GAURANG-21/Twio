bf3e07cbe0ac6a6c977742ccf1fecdd7b47385fd
GAURANG-21 <gaurang2621@gmail.com>
2025-01-26 12:49:27 +0530

    add: route that changes isLive in db with start and end of streaming automatically; modify: public routes

---
+++ app/api/webhooks/livekit/route.ts
@@ -0,0 +1,52 @@
+import { headers } from "next/headers";
+import { WebhookReceiver } from "livekit-server-sdk";
+
+import { db } from "@/lib/db";
+
+const receiver = new WebhookReceiver(
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_SECRET_KEY!
+);
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const headerPayload = await headers();
+  const authorization = headerPayload.get("authorization");
+
+  if (!authorization) {
+    return new Response("Error occured -- no authorization headers", {
+      status: 400,
+    });
+  }
+
+  const event = await receiver.receive(body, authorization);
+
+  console.log(event);
+
+  if (event.event === "ingress_started") {
+    await db.stream.update({
+      where: {
+        ingressId: event.ingressInfo?.ingressId,
+      },
+      data: {
+        isLive: true,
+      },
+    });
+  }
+
+  if (event.event === "ingress_ended") {
+    await db.stream.update({
+      where: {
+        ingressId: event.ingressInfo?.ingressId,
+      },
+      data: {
+        isLive: false,
+      },
+    });
+  }
+
+  return new Response("Success!", { status: 200 });
+}
+
+//forgot to make the public route
+// typing error while writing secret key
