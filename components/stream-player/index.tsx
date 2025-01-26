a7a62fc108fe7fa37c32ef22f43f9f24a07c6bc7
GAURANG-21 <gaurang2621@gmail.com>
2025-01-27 01:50:38 +0530

    add: UI changes when streaming in turned ON or OFF

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,6 +3,9 @@
 import { useViewerToken } from "@/hooks/use-viewer-token";
 import { User, Stream } from "@prisma/client";
 
+import { LiveKitRoom } from "@livekit/components-react";
+import { Video } from "./video";
+
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
   stream: Stream;
@@ -23,5 +26,17 @@ export const StreamPlayer = ({
   if (!token || !name || !identity) {
     return <div>Cannot watch the Stream</div>;
   }
-  return <div>Allowed to watch the stream</div>;
+  return (
+    <>
+      <LiveKitRoom
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+      >
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+      </LiveKitRoom>
+    </>
+  );
 };
