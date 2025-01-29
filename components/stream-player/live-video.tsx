fc0d0e69de61066a72596935737bef22ff75fe37
GAURANG-21 <gaurang2621@gmail.com>
2025-01-30 01:13:06 +0530

    feat: mute and unmute feature

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -4,6 +4,7 @@ import { Participant, Track } from "livekit-client";
 import { useTracks } from "@livekit/components-react";
 import { useRef, useState, useEffect } from "react";
 import { FullScreenControl } from "./fullscreen-control";
+import { VolumeControl } from "./volume-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -14,11 +15,31 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [volume, setVolume] = useState(50); // Set initial volume to 50
+
+  const onVolumeChange = (value: number) => {
+    setVolume(value);
+    if (videoRef.current) {
+      videoRef.current.muted = value === 0;
+      videoRef.current.volume = value * 0.01;
+    }
+  };
+
+  const toggleMute = () => {
+    const isMuted = volume === 0;
+    const newVolume = isMuted ? 50 : 0;
+    setVolume(newVolume);
+
+    if (videoRef.current) {
+      videoRef.current.muted = newVolume === 0;
+      videoRef.current.volume = newVolume * 0.01;
+    }
+  };
 
   const toggleFullScreen = () => {
     if (isFullScreen) {
       document.exitFullscreen();
-    } else if (wrapperRef?.current) {
+    } else if (wrapperRef.current) {
       wrapperRef.current.requestFullscreen();
     }
   };
@@ -27,7 +48,7 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
     const isCurrentlyFullScreen = document.fullscreenElement !== null;
     setIsFullScreen(isCurrentlyFullScreen);
   };
-  
+
   useEffect(() => {
     document.addEventListener("fullscreenchange", handleFullScreenChange);
     return () => {
@@ -48,6 +69,11 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
       <video ref={videoRef} width="100%" />
       <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
         <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <VolumeControl
+            onChange={onVolumeChange}
+            onToggle={toggleMute}
+            value={volume} // Pass the volume state here
+          />
           <FullScreenControl
             isFullScreen={isFullScreen}
             onToggle={toggleFullScreen}
