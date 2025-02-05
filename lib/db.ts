5801cc685b985e1e2d328463fd64ebc7782d10f1
GAURANG-21 <gaurang2621@gmail.com>
2025-02-05 12:45:50 +0530

    fixes: multiple prismaClient instances generation

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global{
-    // eslint-disable-next-line no-var
-    var prisma: PrismaClient | undefined;
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
 }
 
+// Use existing Prisma client or create a new one
+const db = globalThis.prisma ?? new PrismaClient();
 
-//* In development, we don't want a client to be created every time we change the code.
-//* That's why for development, we declare PrismaClient as global as the global does not undergo hot reload.
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
 
-export const db = globalThis.prisma || new PrismaClient();
-
-if(process.env.NODE_ENV!='production') globalThis.prisma = db;
+export { db };
